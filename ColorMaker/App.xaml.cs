87ce3d3227a4753a0c51a720d9f1731080576631
Miguel Ernesto Wells Vazquez <miguelwellspay1@gmail.com>
2025-03-01 18:01:21 -0300

    Atualiza configurações e adiciona bibliotecas AOT
    As alterações no arquivo `App.xaml.cs` adicionaram comentários detalhados sobre como publicar o projeto para diferentes plataformas (Windows, Android e iOS), incluindo comandos específicos para a linha de comando e links para documentação. Comentários foram removidos e substituídos por instruções mais claras e organizadas.
    
    No arquivo `ColorMaker.csproj`, a lista de `TargetFrameworks` foi alterada para incluir `net9.0-android35.0`, e o tipo de pacote do Windows foi mudado de `None` para `MSIX`. Além disso, várias novas propriedades foram adicionadas para configuração de assinatura de pacote e geração de artefatos de teste, como `GenerateAppInstallerFile`, `AppxPackageSigningEnabled`, e `PackageCertificateThumbprint`.
    
    No arquivo `Package.appxmanifest`, o nome do editor foi alterado de "User Name" para "MWTIS-Local", e a versão do pacote foi atualizada de "0.0.0.0" para "1.0.1.0".
    
    No arquivo `launchSettings.json`, o `commandName` para o perfil "Windows Machine" foi alterado de "Project" para "MsixPackage", indicando que agora o projeto deve ser executado como um pacote MSIX em vez de um projeto padrão.
    
    As alterações no arquivo `com.mwtis.colormaker.aab` indicam a adição de várias bibliotecas AOT (Ahead Of Time) essenciais para o funcionamento do aplicativo, incluindo componentes do Xamarin e Android, como `libaot-Microsoft.Maui.Controls.dll.so`, `libaot-Xamarin.AndroidX.AppCompat.dll.so`, e `libaot-Xamarin.Google.Android.Material.dll.so`. Além disso, foram adicionados arquivos de manifesto e recursos que são fundamentais para a configuração do aplicativo em diferentes plataformas. Essas adições visam melhorar o desempenho e a funcionalidade do aplicativo, permitindo que o código seja compilado antes da execução e garantindo que todos os recursos necessários estejam disponíveis. Não foram removidas linhas de código, o que sugere que essas são novas inclusões no projeto.

--- ColorMaker/App.xaml.cs
+++ ColorMaker/App.xaml.cs
@@ -13,24 +13,46 @@
         }
     }
 
-    //PAra publicar el proyecto para Windows se debe usar el siguiente comando en la terminal
+
+    /*
+    Nunca olvidar primero entrar en las propiedades del proyecto para marcar verificar
+    las opciones de android y Windows https://learn.microsoft.com/pt-br/dotnet/maui/windows/setup?view=net-maui-9.0
+    para permitir la publicacion.
+    Siempre que se vaya a publicar un proyecto se debe tener en cuenta que se debe primero compilar el proyecto.
+    */
+
+    //Para publicar el proyecto para Windows se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-windows -r win-x64
+
     //Para publicar el proyecto para Android se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-android -r android
+
     //Para publicar el proyecto para iOS se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-ios -r ios
+
     //Para publicar el proyeto para Windows y colocar en la tienda de Windows se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-windows -r win-x64 -p:PublishSingleFile=true
+
     //Para publicar el proyeto para Android y colocar en la tienda de Google se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-android -r android -p:PublishSingleFile=true
+
     //Para publicar el proyeto para iOS y colocar en la tienda de Apple se debe usar el siguiente comando en la terminal
     //dotnet build -t:Publish -f net6.0-ios -r ios -p:PublishSingleFile=true
+
     // Manuales de publicacion en:
-    //Windows: https://learn.microsoft.com/pt-br/dotnet/maui/windows/deployment/publish?view=net-maui-9.0
+    //Windows:
+    //https://learn.microsoft.com/pt-br/dotnet/maui/windows/deployment/publish?view=net-maui-9.0
     //https://learn.microsoft.com/pt-br/dotnet/maui/windows/deployment/overview?view=net-maui-9.0
-    //Android: https://learn.microsoft.com/pt-br/dotnet/maui/android/deployment/publish?view=net-maui-9.0
+    //Fuera de la tienda, autoassinado con certificado digital de Windows: https://learn.microsoft.com/pt-br/dotnet/maui/windows/deployment/publish-visual-studio-folder?view=net-maui-9.0
+
+
+    //Android:
+    //https://learn.microsoft.com/pt-br/dotnet/maui/android/deployment/publish?view=net-maui-9.0
     //https://learn.microsoft.com/pt-br/dotnet/maui/android/deployment/overview?view=net-maui-9.0
-    //iOS: https://learn.microsoft.com/pt-br/dotnet/maui/ios/deployment/publish?view=net-maui-9.0
+    //https://learn.microsoft.com/pt-br/dotnet/maui/android/deployment/publish-google-play?view=net-maui-9.0
+
+    //iOS:
+    //https://learn.microsoft.com/pt-br/dotnet/maui/ios/deployment/publish?view=net-maui-9.0
     //https://learn.microsoft.com/pt-br/dotnet/maui/ios/deployment/overview?view=net-maui-9.0
     //https://learn.microsoft.com/pt-br/dotnet/maui/ios/deployment/publish?view=net-maui-9.0
 
